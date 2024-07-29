cef814dc383831adeecafa0b3089a9c7bb97df08
Trey Shaw <treyshaw81@gmail.com>
2024-07-28 20:33:50 -0500

    cant use keys, remove toggle and replace with command to use smart depo

--- src/main/java/io/github/toomanybugs1/smartdeposit/SmartDeposit.java
+++ src/main/java/io/github/toomanybugs1/smartdeposit/SmartDeposit.java
@@ -1,7 +1,5 @@
 package io.github.toomanybugs1.smartdeposit;
 
-import java.util.*;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,44 +9,37 @@ import net.md_5.bungee.api.ChatColor;
 
 public class SmartDeposit extends JavaPlugin {
 	
-	List<String> enabledPlayers;
-	
 	@Override
     public void onEnable() {
-		this.saveDefaultConfig();
+
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (!cmd.getName().equalsIgnoreCase("togglesmartdeposit") || args.length != 0)
+        if (!cmd.getName().equalsIgnoreCase("sd") || args.length != 0)
             return false;
 
         if (sender instanceof Player) {
 
-            this.enabledPlayers = this.getConfig().getStringList("players-enabled");
-
-            String playerName = sender.getName();
-
-            if (this.enabledPlayers.contains(playerName)) {
-                this.enabledPlayers.remove(playerName);
-
-                sender.sendMessage(ChatColor.GOLD + "[SmartDeposit] "
-                    + ChatColor.DARK_RED + "Smart depositing disabled.");
-            } else {
-                this.enabledPlayers.add(playerName);
-
-                sender.sendMessage(ChatColor.GOLD + "[SmartDeposit] "
-                    + ChatColor.DARK_GREEN + "Smart depositing enabled.");
-            }
-
-            this.getConfig().set("players-enabled", this.enabledPlayers);
-            this.saveConfig();
+        	if (this.smartDeposit((Player) sender)) {
+        		sender.sendMessage(ChatColor.GOLD + "[SmartDeposit] "
+                        + ChatColor.DARK_GREEN + "Deposited inventory successfully.");
+        	}
+        	else {
+        		sender.sendMessage(ChatColor.GOLD + "[SmartDeposit] "
+                        + ChatColor.DARK_RED + "Deposited inventory successfully.");
+        	}
+            
         } else {
             sender.sendMessage("Only players can use this command.");
         }
 
 
         return true;
+    }
+    
+    private boolean smartDeposit(Player player) {
+    	return true;
     }
 }
